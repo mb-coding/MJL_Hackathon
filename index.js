8d0c3911c84b44f069213b5961356f0fe6c6c1bd
ASNikolay07 <07asnikolay@gmail.com>
2026-02-08 12:46:51 -0600

    Changed plan for integrating the game

--- index.js
+++ index.js
@@ -131,19 +131,6 @@ function startQuiz(){
     incrementQuestion();
 }
 
-//Helper function meant to open a timed window with the game
-function openGameWindow(){
-    const url = 'game.html';
-
-    const gameWindow = window.open(url);
-
-    if (gameWindow){
-        gameWindow.focus();
-        setTimeout(gameWindow.close(), 5000);
-    } else {
-        alert('Error: could not load game :(');
-    }
-}
 
 // start of program
 document.getElementById("introHeading").innerText = "Welcome to the reading app!";
