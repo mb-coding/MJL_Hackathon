b0e671f7bea6054d585dd778aa3bb0c209c25561
MBritto <109992316+mb-coding@users.noreply.github.com>
2026-02-07 23:19:51 -0600

    Added function to clear intro screen

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-const iSurvivedQuestionsParagraph1 = ["question 1", "question 2", "question 3", "question 4"];
-const iSurvivedQuestionsParagraph2 = ["question 1", "question 2", "question 3", "question 4"];
-const iSurvivedQuestionsParagraph3 = ["question 1", "question 2", "question 3", "question 4"];
-const iSurvivedQuestionsParagraph4 = ["question 1", "question 2", "question 3", "question 4"];
-const iSurvivedQuestionsParagraph5 = ["question 1", "question 2", "question 3", "question 4"];
-const iSurvivedAnswersParagraph1 = ["answer 1", "answer 2", "answer 3", "answer 4"];
-const iSurvivedAnswersParagraph2 = ["answer 1", "answer 2", "answer 3", "answer 4"];
-const iSurvivedAnswersParagraph3 = ["answer 1", "answer 2", "answer 3", "answer 4"];
-const iSurvivedAnswersParagraph4 = ["answer 1", "answer 2", "answer 3", "answer 4"];
-const iSurvivedAnswersParagraph5 = ["answer 1", "answer 2", "answer 3", "answer 4"];
+// lists of questions
+let iSurvivedQuestionsParagraph1 = ["question 1", "question 2", "question 3", "question 4"];
+let iSurvivedQuestionsParagraph2 = ["question 1", "question 2", "question 3", "question 4"];
+let iSurvivedQuestionsParagraph3 = ["question 1", "question 2", "question 3", "question 4"];
+let iSurvivedQuestionsParagraph4 = ["question 1", "question 2", "question 3", "question 4"];
+let iSurvivedQuestionsParagraph5 = ["question 1", "question 2", "question 3", "question 4"];
 
-let iSurvivedParagraph1 = "This is the first paragraph of the book.";
-let iSurvivedParagraph2 = "This is the second paragraph of the book.";
-let iSurvivedParagraph3 = "This is the third paragraph of the book.";
-let iSurvivedParagraph4 = "This is the fourth paragraph of the book.";
-let iSurvivedParagraph5 = "This is the fifth paragraph of the book.";
+// lists of answers
+let iSurvivedAnswersParagraph1 = ["answer 1", "answer 2", "answer 3", "answer 4"];
+let iSurvivedAnswersParagraph2 = ["answer 1", "answer 2", "answer 3", "answer 4"];
+let iSurvivedAnswersParagraph3 = ["answer 1", "answer 2", "answer 3", "answer 4"];
+let iSurvivedAnswersParagraph4 = ["answer 1", "answer 2", "answer 3", "answer 4"];
+let iSurvivedAnswersParagraph5 = ["answer 1", "answer 2", "answer 3", "answer 4"];
 
-document.getElementById("heading").innerText = "Welcome to the reading app!";
-document.getElementById("heading2").innerText = "What book would you like to read today?"
-document.getElementById("paragraph").innerText = iSurvivedParagraph1;
+// lists of paragraphs
+const iSurvivedParagraph1 = "This is the first paragraph of the book.";
+const iSurvivedParagraph2 = "This is the second paragraph of the book.";
+const iSurvivedParagraph3 = "This is the third paragraph of the book.";
+const iSurvivedParagraph4 = "This is the fourth paragraph of the book.";
+const iSurvivedParagraph5 = "This is the fifth paragraph of the book.";
+
+// function to clear the screen of all html elements
+function clearIntroScreen(){
+    document.getElementById("introHeading").innerHTML = "";
+    document.getElementById("introHeading2").innerHTML = "";
+    document.getElementById("introButton").remove();
+}
+
+// function to show text after book is selected
+function showParagraphs(){
+    clearIntroScreen()
+}
+
+// start of program
+document.getElementById("introHeading").innerText = "Welcome to the reading app!";
+document.getElementById("introHeading2").innerText = "What book would you like to read today?";
+
+
