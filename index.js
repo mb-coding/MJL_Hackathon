b35975498ba8144600df1c72f85aa4203a3b6052
MBritto <109992316+mb-coding@users.noreply.github.com>
2026-02-08 09:48:07 -0600

    Started startQuiz function and worked on updating question bank

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 // lists of questions
-let iSurvivedQuestionsParagraph1 = ["question 1", "question 2", "question 3", "question 4"];
-let iSurvivedQuestionsParagraph2 = ["question 1", "question 2", "question 3", "question 4"];
-let iSurvivedQuestionsParagraph3 = ["question 1", "question 2", "question 3", "question 4"];
-let iSurvivedQuestionsParagraph4 = ["question 1", "question 2", "question 3", "question 4"];
-let iSurvivedQuestionsParagraph5 = ["question 1", "question 2", "question 3", "question 4"];
-let allISurvivedQuestions = [...iSurvivedQuestionsParagraph1,...iSurvivedQuestionsParagraph2,...iSurvivedQuestionsParagraph3,...iSurvivedQuestionsParagraph4,...iSurvivedQuestionsParagraph5];
+let iSurvivedQuestionsParagraph1 = ["Who was Josh navigating the fire with?", "What did the glowing embers and chunks of flaming wood feel like to Josh?"];
+let iSurvivedQuestionsParagraph2 = ["What happened to the tree?", "Where was the tree branch heading?"];
+let iSurvivedQuestionsParagraph3 = ["Where did Josh's dad work?", "How long did Josh wait for his mom?"];
+// let iSurvivedQuestionsParagraph4 = ["question 1", "question 2", "question 3", "question 4"];
+// let iSurvivedQuestionsParagraph5 = ["question 1", "question 2", "question 3", "question 4"];
+// let allISurvivedQuestions = [...iSurvivedQuestionsParagraph1,...iSurvivedQuestionsParagraph2,...iSurvivedQuestionsParagraph3,...iSurvivedQuestionsParagraph4,...iSurvivedQuestionsParagraph5];
+let allISurvivedQuestions = [...iSurvivedQuestionsParagraph1,...iSurvivedQuestionsParagraph2,...iSurvivedQuestionsParagraph3];
+
 
 // lists of answers
-let iSurvivedAnswersParagraph1 = ["answer 1", "answer 2", "answer 3", "answer 4"];
-let iSurvivedAnswersParagraph2 = ["answer 1", "answer 2", "answer 3", "answer 4"];
-let iSurvivedAnswersParagraph3 = ["answer 1", "answer 2", "answer 3", "answer 4"];
-let iSurvivedAnswersParagraph4 = ["answer 1", "answer 2", "answer 3", "answer 4"];
-let iSurvivedAnswersParagraph5 = ["answer 1", "answer 2", "answer 3", "answer 4"];
-let allISurvivedAnswers = [...iSurvivedAnswersParagraph1,...iSurvivedAnswersParagraph2,...iSurvivedAnswersParagraph3,...iSurvivedAnswersParagraph4,...iSurvivedAnswersParagraph5];
+let iSurvivedAnswersParagraph1Question1 = ["Cousin Holly", "Greg", "Josh's mom", "John's aunt"];
+let iSurvivedAnswersParagraph1Question2 = ["A flesh eating monster", "A sharped tooth shark", "The claws of a bear", "Sharp shards of glass"];
+let iSurvivedAnswersParagraph2Question1 = ["It exploded", "It broke", "It flew away", "Someone crashed into it"];
+let iSurvivedAnswersParagraph2Question2 = ["For Josh's skull", "For Holly's skull", "For a building", "For Josh's mom"];
+let iSurvivedAnswersParagraph3Question1 = ["A big New York City bank", "The NBA", "The Airport", "A university"];
+let iSurvivedAnswersParagraph3Question2 = ["10 minutes", "5 minutes", "20 minutes", "60 minutes"];
+// let iSurvivedAnswersParagraph4 = ["answer 1", "answer 2", "answer 3", "answer 4"];
+// let iSurvivedAnswersParagraph5 = ["answer 1", "answer 2", "answer 3", "answer 4"];
+// let allISurvivedAnswers = [...iSurvivedAnswersParagraph1,...iSurvivedAnswersParagraph2,...iSurvivedAnswersParagraph3,...iSurvivedAnswersParagraph4,...iSurvivedAnswersParagraph5];
+let allISurvivedAnswers = [...iSurvivedAnswersParagraph1,...iSurvivedAnswersParagraph2,...iSurvivedAnswersParagraph3];
 
 
 // lists of paragraphs
-const iSurvivedParagraph1 = "This is the first paragraph of the book.";
+const iSurvivedParagraph1 = "Josh stared in horror at the blood-red glow rising up behind him. A massive wildfire was raging through the forest, a wall of flames devouring everything in its path. Josh and his cousin Holly were in a race for their lives.\n\t`Josh!` Holly shouted, grabbing his hand.\n`This way!`\n\tAs they took off along the forest path, the hot wind gusted hard. Suddenly,the air was filled with sparks and glowing embers and chunks of flaming wood. Josh felt them landing on his bare arms and legs, sizzling against his skin like the white-hot teeth of a flesh-eating monster.\n\tHe and Holly ran faster, but the wall of flames was closing in from behind. And all around them, the burning embers were setting fires wherever they landed.";
 const iSurvivedParagraph2 = "This is the second paragraph of the book.";
 const iSurvivedParagraph3 = "This is the third paragraph of the book.";
-const iSurvivedParagraph4 = "This is the fourth paragraph of the book.";
-const iSurvivedParagraph5 = "This is the fifth paragraph of the book.";
-const allISurvivedParagraphs = [iSurvivedParagraph1,iSurvivedParagraph2,iSurvivedParagraph3,iSurvivedParagraph4,iSurvivedParagraph5];
+// const iSurvivedParagraph4 = "This is the fourth paragraph of the book.";
+// const iSurvivedParagraph5 = "This is the fifth paragraph of the book.";
+// const allISurvivedParagraphs = [iSurvivedParagraph1,iSurvivedParagraph2,iSurvivedParagraph3,iSurvivedParagraph4,iSurvivedParagraph5];
+const allISurvivedParagraphs = [iSurvivedParagraph1,iSurvivedParagraph2,iSurvivedParagraph3];
+
+// list of choices
+const iSurvivedParagraph1Choices = [["choice 1", "choice 2", "choice 3", "choice 4"], ["choice 1, choice 2, choice 3, choice 4"], ["choice 1, choice 2, choice 3"], ["choice 1", "choice 2", "choice 3", "choice 4"], ["choice 1", "choice 2", "choice 3", "choice 4"]];
+const iSurvivedParagraph2Choices = [["choice 1", "choice 2", "choice 3", "choice 4"], ["choice 1, choice 2, choice 3, choice 4"], ["choice 1, choice 2, choice 3"], ["choice 1", "choice 2", "choice 3", "choice 4"], ["choice 1", "choice 2", "choice 3", "choice 4"]];
+const iSurvivedParagraph3Choices = [["choice 1", "choice 2", "choice 3", "choice 4"], ["choice 1, choice 2, choice 3, choice 4"], ["choice 1, choice 2, choice 3"], ["choice 1", "choice 2", "choice 3", "choice 4"], ["choice 1", "choice 2", "choice 3", "choice 4"]];
+// const iSurvivedParagraph4Choices = [["choice 1", "choice 2", "choice 3", "choice 4"], ["choice 1, choice 2, choice 3, choice 4"], ["choice 1, choice 2, choice 3"], ["choice 1", "choice 2", "choice 3", "choice 4"], ["choice 1", "choice 2", "choice 3", "choice 4"]];
+// const iSurvivedParagraph5Choices = [["choice 1", "choice 2", "choice 3", "choice 4"], ["choice 1, choice 2, choice 3, choice 4"], ["choice 1, choice 2, choice 3"], ["choice 1", "choice 2", "choice 3", "choice 4"], ["choice 1", "choice 2", "choice 3", "choice 4"]];
+
+// list of correct/incorrect
+const iSurvivedParagraph1CorrectIncorrect = [["correct", "correct", "correct", "incorrect"], ["correct, correct, correct, incorrect"], ["correct, correct, incorrect"], ["correct", "correct", "correct", "incorrect"], ["correct", "correct", "correct", "correct"]];
+const iSurvivedParagraph2CorrectIncorrect = [["correct", "correct", "correct", "incorrect"], ["correct, correct, correct, incorrect"], ["correct, correct, incorrect"], ["correct", "correct", "correct", "incorrect"], ["correct", "correct", "correct", "correct"]];
+const iSurvivedParagraph3CorrectIncorrect = [["correct", "correct", "correct", "incorrect"], ["correct, correct, correct, incorrect"], ["correct, correct, incorrect"], ["correct", "correct", "correct", "incorrect"], ["correct", "correct", "correct", "correct"]];
+// const iSurvivedParagraph4CorrectIncorrect = [["correct", "correct", "correct", "incorrect"], ["correct, correct, correct, incorrect"], ["correct, correct, incorrect"], ["correct", "correct", "correct", "incorrect"], ["correct", "correct", "correct", "correct"]];
+// const iSurvivedParagraph5CorrectIncorrect = [["correct", "correct", "correct", "incorrect"], ["correct, correct, correct, incorrect"], ["correct, correct, incorrect"], ["correct", "correct", "correct", "incorrect"], ["correct", "correct", "correct", "correct"]];
 
 let numOfParagraphs = 5;
 
@@ -50,19 +71,19 @@ function readInput(){
     // console.log(document.getElementById("numParagraphsText").value);
     let text = document.getElementById("numParagraphsText").value
     // change these values if the number of paragraphs change
-    if (text=="2" || text == "3" || text == "4" || text == "5") {
+    if (text=="2" || text == "3") {
         if (text=="2"){
             selectedNumOfParagraphs = 2;
         }
         else if (text == "3"){
             selectedNumOfParagraphs = 3;
         }
-        else if (text == "4"){
-            selectedNumOfParagraphs = 4;
-        }
-        else if (text == "5"){
-            selectedNumOfParagraphs = 5;
-        }
+        // else if (text == "4"){
+        //     selectedNumOfParagraphs = 4;
+        // }
+        // else if (text == "5"){
+        //     selectedNumOfParagraphs = 5;
+        // }
         setupToShowParagraphs();
     }
     else {
@@ -100,7 +121,19 @@ function increment(){
 
 // function to begin quiz
 function startQuiz(){
-
+    document.getElementById("questionHeading").classList.remove("hidden")
+    document.getElementById("questions").classList.remove("hidden");
+    document.getElementById("submitButton").classList.remove("hidden");
+    listOfQuestions = iSurvivedQuestionsParagraph1+iSurvivedQuestionsParagraph2
+    if(selectedNumOfParagraphs==3){
+        listOfQuestions += iSurvivedQuestionsParagraph3;
+    }
+    // else if(selectedNumOfParagraphs == 4){
+    //     listOfQuestions += iSurvivedQuestionsParagraph4;
+    // }
+    // else if(selectedNumOfParagraphs == 5){
+    //     listOfQuestions += iSurvivedQuestionsParagraph5;
+    // }
 }
 
 // start of program
